6eca740ae76130ae43bcbe4c571a7f3946bb14b0
Alfredo <alfredodirksen130@gmail.com>
2022-09-28 09:27:53 +0200

    db toegevoegd

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "snackbar";
+$mysqli = new mysqli($host, $dbuser, $dbpass, $dbname);
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+?>
